4c2703b5a0d1cf65a98e411f59fdfc0646582bf3
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-26 10:02:06 -0600

    getting parts together

---
+++ regex.js
@@ -0,0 +1,5 @@
+var "baseUrl: "https://www.youtube.com/api/timedtext?v=r7SO-Oq3d5E\u0026asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru\u0026caps=asr\u0026exp=xftt\u0026xorp=true\u0026xoaf=5\u0026hl=en\u0026ip=0.0.0.0\u0026ipbits=0\u0026expire=1609021990\u0026sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf\u0026signature=096AF61FD9E37FA27E0CC3CE59B271ABCA812290.78AE4E92A20C3F36EECD634F9FA0C2DEC53EE033\u0026key=yt8\u0026kind=asr\u0026lang=en"
+
+"playerCaptionsTracklistRenderer":{"captionTracks":[{"baseUrl":"https://www.youtube.com/api/timedtext?v=r7SO-Oq3d5E\u0026asr_langs=de,en,es,fr,it,ja,ko,nl,pt"
+
+var regexp = new RegExp(/playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/)
